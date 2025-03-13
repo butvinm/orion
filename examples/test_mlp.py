86373b5759c81d37f7e06734d32b322f45d45fe4
Austin Ebel <abe5240@nyu.edu>
2025-03-13 16:29:20 -0400

    Update example [no ci]

--- examples/test_mlp.py
+++ examples/test_mlp.py
@@ -1,45 +1,56 @@
 import time
 import math
-
 import torch
 import orion
-from orion.core.utils import get_mnist_datasets, mae, train_on_mnist
-
 import models
+from orion.core.utils import (
+    get_mnist_datasets,
+    mae, 
+    train_on_mnist
+)
 
+# Set seed for reproducibility
 torch.manual_seed(42)
+
+# Initialize model and data
 scheme = orion.init_scheme("test_parameters.yaml")
-
 batch_size = scheme.get_batch_size()
-trainloader, testloader = get_mnist_datasets(
-    data_dir="../data", batch_size=batch_size)
-
+trainloader, testloader = get_mnist_datasets(data_dir="../data", batch_size=batch_size)
 net = models.MLP()
-#train_on_mnist(net, data_dir="../data", epochs=1, device="cuda")
 
+# Train model (optional)
+train_on_mnist(net, data_dir="../data", epochs=1, device="cuda")
+
+# Get test batch for inference comparison
 inp, _ = next(iter(testloader))
+
+# Run cleartext inference
 net.eval()
+out_clear = net(inp)
 
-out_clear = net(inp) # Cleartext inference
-
-# Fit and compile (temporarily increase batch size for speed)
-orion.fit(net, trainloader, batch_size=128)
+# Prepare for FHE inference. 
+# We'll determine the ranges with which to fit each polynomial activation
+# function to by gathering these statistics from the training set and 
+# applying a tolerance factor = margin.
+orion.fit(net, trainloader, batch_size=128, margin=2)
 input_level = orion.compile(net)
 
-# Encode and encrypt
+# Encode and encrypt the input vector 
 vec_ptxt = orion.encode(inp, input_level)
 vec_ctxt = orion.encrypt(vec_ptxt)
+net.he()  # Switch to FHE mode
 
-net.he() # Set model to FHE inference mode
-
+# Run FHE inference
 print("\nStarting FHE inference", flush=True)
 start = time.time()
-out_ctxt = net(vec_ctxt) # perform FHE inference
+out_ctxt = net(vec_ctxt)
 end = time.time()
 
+# Get the FHE results and decrypt + decode.
 out_ptxt = out_ctxt.decrypt()
 out_fhe = out_ptxt.decode()
 
+# Compare the cleartext and FHE results.
 print()
 print(out_clear)
 print(out_fhe)
